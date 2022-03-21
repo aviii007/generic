e0e5b355093f2289e91ccd4e66765ff883ad2909
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-21 17:12:32 +0530

    Added the welcome message.

---
+++ src/com/bridgelabz/arraytypes/printArrays.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.arraytypes;
+
+public class printArrays {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to printing array of different data types in generics");
+    }
+}

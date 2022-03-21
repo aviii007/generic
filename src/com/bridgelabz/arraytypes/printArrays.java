ba8b018d4292321d4b9391327ae2e7b411befc86
Ashvini Kanojia <kanojia.avi@gmail.com>
2022-03-21 17:15:32 +0530

    Added the array of different data types.

--- src/com/bridgelabz/arraytypes/printArrays.java
+++ src/com/bridgelabz/arraytypes/printArrays.java
@@ -1,8 +1,26 @@
 package com.bridgelabz.arraytypes;
 
 public class printArrays {
+    public static <E> void printArray(E[] inputArray) {
+        for (E element : inputArray)
+            System.out.printf("%s ", element);
+        System.out.println();
+
+    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to printing array of different data types in generics");
+        Integer[] integerArray = {1, 2, 3, 4, 5, 6, 7};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7};
+        Character[] characterArray = {'A', 'S', 'H', 'V', 'I', 'N', 'I'};
+
+        System.out.println("The Array of integerArray has:");
+        printArray(integerArray);
+
+        System.out.println("The Array of doubleArray has:");
+        printArray(doubleArray);
+
+        System.out.println("The Array of characterArray has:");
+        printArray(characterArray);
     }
 }
+
